09da4438bd46e783570f721fcc86e9fd55ee72f1
Anthony Baker <abake48@gmail.com>
2024-04-26 11:16:10 -0600

    changed default flake.nix

--- flake.nix
+++ flake.nix
@@ -3,13 +3,24 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    
+    #home-manager = {
+    #  url = "github:nix-community/home-manager";
+    #  input.nixpkgs.follows = "nixpkgs";
+    #}; 
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        extraSpecialArgs = {inherit inputs;};
+        modules = [
+          /etx/nixos/configuration.nix
+          #inputs.home-manager.nixosModules.default
+        ];
+      };
   };
 }
