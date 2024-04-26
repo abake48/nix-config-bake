6acffa4446d3677fd98561121b412e8c2f9d4253
Anthony Baker <abake48@gmail.com>
2024-04-26 15:51:04 -0600

    adding home manager

--- flake.nix
+++ flake.nix
@@ -4,23 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     
-    #home-manager = {
-    #  url = "github:nix-community/home-manager";
-    #  input.nixpkgs.follows = "nixpkgs";
-    #}; 
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    }; 
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-        extraSpecialArgs = {inherit inputs;};
-        modules = [
-          /etx/nixos/configuration.nix
-          #inputs.home-manager.nixosModules.default
-        ];
-      };
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ../configuration.nix
+        inputs.home-manager.nixosModules.default
+      ];
+    };
   };
 }
